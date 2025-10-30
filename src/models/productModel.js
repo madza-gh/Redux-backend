91d776a02f436efacec783cdeac83ad611ae523a
madza <mr.ghasemi.1375@gmail.com>
2025-10-30 13:42:12 +0330

    product model

---
+++ src/models/productModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        price:{
+            type: Number,
+            required: true,
+            min: 0
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
