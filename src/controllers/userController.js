26bbc3232ff2b67fa54603a09d0940013bfe998c
madza <mr.ghasemi.1375@gmail.com>
2025-11-08 15:15:50 +0330

    developing usercontroller

---
+++ src/controllers/userController.js
@@ -0,0 +1,55 @@
+import { User } from "../models/userModel";
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "30d"})
+}
+
+export const registerUser = async(req, res) =>{
+    const {name, email, password} = req.body
+
+    try {
+        const userExists = await User.findOne({email})
+        if(userExists) return res.status(400).json({message: "user already exists"})
+
+        const user = await User.create({name, email, password})
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+export const loginUser = async(req, res) =>{
+    const {email, password} = req.body
+
+    try {
+        const user = await User.findOne({email})
+        if(user && (await User.matchPassword(password))){
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token:generateToken(user._id)
+
+            })
+        }else {
+            res.status(401).json({message: "invalid email or password"})
+        }
+
+    } catch (error) {
+        res.status(500).json({message: error.message})        
+    }
+}
+
+export const getUserProfile = async (req, res) => {
+    const user = req.user
+    
+    if(user) res.json(user)
+    else res.status(404).json({message: "User not found!"})
+}
